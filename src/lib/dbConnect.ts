7c8a921bb96ddfa9a7f90aff336bb9399039644a
Abdul-Wahab08 <abdulwahab3929706@gmail.com>
2026-03-02 16:50:54 +0500

    resolve bug in database connnection

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -13,7 +13,7 @@ const dbConnect = async (): Promise<void> => {
     }
 
     try {
-        const db = await mongoose.connect(process.env.MONGODB_URI || '')
+        const db = await mongoose.connect(process.env.MONGODB_URI! )
         connection.isConnected = db.connections[0].readyState
         console.log("Database connected Successfully")
     } catch (error) {
